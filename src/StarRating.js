46f349742be021dbee270faefe48911a0cdf3325
Murali981 <muraliju981@gmail.com>
2024-10-12 06:11:13 +0530

    Basic building of the stars component and the layout part of the project is completed

---
+++ src/StarRating.js
@@ -0,0 +1,133 @@
+import { useState } from "react";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center", // Align  the items vertically centered
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+// To set the default value of maxRating , we can leverage the power of destructuring in javascript because whenever we destructure an
+// object then we can actually set a default value. In the below we are destructuring the props object "maxRating" and we can set the
+// default value for maxRating to 5 if maxRating doesn't exist ({maxRating = 5})
+export default function StarRating({ maxRating = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    // Created a handler function that owns the state and then passing this handler function into some other child component which is
+    // the Star component which will actually update the state
+    setRating(rating);
+  }
+
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          // The above "i" comes from index of the empty array that we create here with the length of the maxRating. So we create this array
+          // and then we immediately loop over it and then the first star that we create has the number "0"(zero) and now we will use this
+          // index inorder to handle our rating. So When we click on the first star then it will be (o+1) = 1 and if we click on the
+          // third star then the index on the third star will be 2 but (i + 1) makes it (2 + 1) = 3 then the rating will set to three.This is
+          // all about setting the rating variable . Now we will see how to determine whether a star is full (or) not ? All we need to do
+          // is compare the currently set rating with the index of the current star.
+          <Star
+            key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+          /> // We need to listen for a click event on each of these stars
+        ))}
+      </div>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
+      {/* "rating || '' " => This is short circuiting where we will move to the second part of the operator whenever there is a 
+      falsy value like 0 but if you have 1 then it is a truthy value */}
+    </div>
+  );
+}
+
+const starStyle = {
+  width: "48px",
+  height: "48px",
+  display: "block",
+  cursor: "pointer",
+};
+
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
+  return (
+    // The below role="button" which is just HTML for accessebility and this is nothing to do with react
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+      {full ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="#000"
+          stroke="#000"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#000"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="{2}"
+            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+          />
+        </svg>
+      )}
+    </span>
+  );
+}
+
+/*
+FULL STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#000"
+  stroke="#000"
+>
+  <path
+    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  />
+</svg>
+
+
+EMPTY STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#000"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  />
+</svg>
+
+*/
